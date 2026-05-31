6798e0f3eeae62ab32fd3c7be6a4e178a3d0f896
Arthur henrique <melo78277@gmail.com>
2026-05-31 15:14:52 -0300

    segundo commit do js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let conta = 0
 
 let pessoas = 0
 
+let porcentagem = 0
+
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
 
@@ -14,15 +16,24 @@ pessoasInput.addEventListener("input", receberQuantidadePessoas)
 
 function receberQuantidadePessoas(evento) {
 
-    const paragrafoErro = document.querySelector(".pessoas #erro")
-    const divErro = document.querySelector(".pessoas .input-box")
+const paragrafoErro = document.querySelector(".pessoas #erro")
+const divErro = document.querySelector(".pessoas .input-box")
 
-    if (evento.target.value === "0") {
-        paragrafoErro.style.display = "block"
-        divErro.setAttribute("id", "erro-div")
-    } else {
-        paragrafoErro.style.display = "none"
-        divErro.setAttribute("id", "")
-        pessoas = Number(evento.target.value)
-    }
+if (evento.target.value === "0") {
+ paragrafoErro.style.display = "block"
+ divErro.setAttribute("id", "erro-div")
+ } else {
+ paragrafoErro.style.display = "none"
+ divErro.setAttribute("id", "")
+pessoas = Number(evento.target.value)
+ }
+}
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagem)
+})
+
+function receberPorcentagem(evento) {
+    console.log(evento.target.value)
 }
